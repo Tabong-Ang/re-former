0cec6a6989e1c883ef9d9436c283edf1c333a69f
Tabong-Ang <tabongphilip@gmail.com>
2025-11-18 09:54:18 +0100

    built crate action to take user parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,10 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params[:user])
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
